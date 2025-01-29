3811f655c80bd23f198e14c36f5264f2bed3abef
De Moura Gabriel <gabriel.demoura@imtf.com>
2025-01-29 15:57:54 +0100

    Custom button with render example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import "./App.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef,  } from "react";
 import WebViewer from "@pdftron/webviewer";
 
+const SavedInfo = ({ color }) => (
+  <div style={{ width: "46px", textAlign: "center" }}>
+    <svg
+      fill={color}
+      width="24"
+      height="24"
+      viewBox="0 0 1792 1792"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z" />
+    </svg>
+  </div>
+);
+
 function App() {
+
   const viewer = useRef(null);
-  const instance = useRef(null);
+  const test = useRef(null);
 
   useEffect(() => {
-    if (!instance.current) {
-      instance.current = "test";
+    if (!test.current) {
+      test.current = "test";
       WebViewer(
         {
           path: "/webviewer",
-          licenseKey: "YOUR_LICENSE_KEY",
-          initialDoc: "/test.doc",
+          licenseKey: "Informatique-MTF SA (imtf.com):OEM:HYPERSUITE/5 Folder Manager::B+:AMS(20240830):E7A5EDAD0477780A0360B13AC982527860613FE98B14F592A9C7D6C24A7220DE22DABEF5C7",
+          initialDoc: "/test.pdf",
         },
         viewer.current
-      );
+      ).then((instance) => {
+        instance.UI.setHeaderItems(header => {
+          header.push({
+            dataElement: "savedButton",
+            type: "customElement",
+            render: () => (
+              <SavedInfo color="green" />
+            ),
+          });
+        });
+      });
     }
   }, []);
 
